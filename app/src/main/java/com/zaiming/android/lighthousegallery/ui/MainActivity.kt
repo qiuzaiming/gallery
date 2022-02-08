15f385fd07346715b9e4046014221a27242cc750
qiuzaiming <qiuzaiming@yulong.com>
2022-02-08 15:08:11 +0800

    features:add request permission
    Signed-off-by: qiuzaiming <qiuzaiming@yulong.com>

--- app/src/main/java/com/zaiming/android/lighthousegallery/ui/MainActivity.kt
+++ app/src/main/java/com/zaiming/android/lighthousegallery/ui/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.zaiming.android.lighthousegallery.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -17,6 +18,12 @@ import com.zaiming.android.lighthousegallery.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val needRequestMultiplePermission = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        if (it.values.any { value -> value == false }) {
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +31,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView = binding.navView
+        requestPermissionLauncher.launch(needRequestMultiplePermission)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(R.id.navigation_photos, R.id.navigation_albums, R.id.navigation_selected, R.id.navigation_recommends)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.labelVisibilityMode = LABEL_VISIBILITY_LABELED
-        navView.setupWithNavController(navController)
+
+        initBottomNavigationView(navController)
+
+    }
+
+    private fun initBottomNavigationView(navController: NavController) {
+        binding.navView.apply {
+            labelVisibilityMode = LABEL_VISIBILITY_LABELED
+            setupWithNavController(navController)
+        }
+
+        //activity listens to different fragments in the foreground
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            TODO()
+        }
     }
 }
