2d0c907ad248a4c3e4f6bd470143d3c2ad8fd13f
qiuzaiming <qiuzaiming@yulong.com>
2022-02-23 20:59:35 +0800

    dependency app center to capture crash
    Signed-off-by: qiuzaiming <qiuzaiming@yulong.com>

--- app/src/main/java/com/zaiming/android/lighthousegallery/GalleryApplication.kt
+++ app/src/main/java/com/zaiming/android/lighthousegallery/GalleryApplication.kt
@@ -1,8 +1,13 @@
 package com.zaiming.android.lighthousegallery
 
 import android.app.Application
+import android.content.pm.PackageManager
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+
 
 /**
  * @author zaiming
@@ -19,12 +24,21 @@ class GalleryApplication : Application() {
         instance = this
 
         initTimber()
+
+        initAppCenter()
     }
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+    }
+
+    private fun initAppCenter() {
+        AppCenter.start(
+            this, packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA).metaData.getString("secret_appcenter"),
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
 }
