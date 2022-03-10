2e48ee433f39877adacf065eb6e66823ff43ff37
qiuzaiming <qiuzaiming@yulong.com>
2022-03-10 18:54:53 +0800

    add ProcessPhoenix to restart app
    Signed-off-by: qiuzaiming <qiuzaiming@yulong.com>

---
+++ app/src/main/java/com/zaiming/android/lighthousegallery/app/ProcessPhoenix.kt
@@ -0,0 +1,19 @@
+package com.zaiming.android.lighthousegallery.app
+
+import android.content.Context
+import android.content.Intent
+import java.lang.IllegalStateException
+
+/**
+ * from drakeet
+ */
+object ProcessPhoenix {
+
+    fun triggerRebirth(context: Context, intent: Intent? = null) {
+        val restartIntent = intent ?: context.packageManager.getLaunchIntentForPackage(context.packageName)
+        ?: throw IllegalStateException("No launcher intent for: ${context.packageName}")
+        restartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        context.startActivity(restartIntent)
+        Runtime.getRuntime().exit(0) // kill kill kill
+    }
+}
