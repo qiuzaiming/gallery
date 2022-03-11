bcca7af32d79fd51616452890d13752394ed9f3d
qiuzaiming <qiuzaiming@yulong.com>
2022-03-11 13:41:58 +0800

    add doOnMainThreadIdle function
    Signed-off-by: qiuzaiming <qiuzaiming@yulong.com>

--- app/src/main/java/com/zaiming/android/lighthousegallery/app/Global.kt
+++ app/src/main/java/com/zaiming/android/lighthousegallery/app/Global.kt
@@ -1,7 +1,9 @@
 package com.zaiming.android.lighthousegallery.app
 
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.os.MessageQueue
 import android.util.Log
 import timber.log.Timber
 
@@ -25,5 +27,31 @@ object Global {
         val stackTraceContent = Log.getStackTraceString(e)
         Timber.e(e)
         throw e
+    }
+
+    /**
+     * from drakeet
+     */
+    fun doOnMainThreadIdle(action: () -> Unit) {
+
+        val idleHandler = MessageQueue.IdleHandler {
+            handler.removeCallbacksAndMessages(null)
+            action()
+            false
+        }
+
+        fun setupIdleHandler(queue: MessageQueue) {
+            queue.addIdleHandler(idleHandler)
+        }
+
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+           setupIdleHandler(Looper.myQueue())
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setupIdleHandler(Looper.getMainLooper().queue)
+            } else {
+                handler.post { setupIdleHandler(Looper.myQueue()) }
+            }
+        }
     }
 }
