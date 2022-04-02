66e354b62326e6a4edc311483292d639ddfc9f40
qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>
2022-04-02 15:00:56 +0800

    add totheme constraint
    Signed-off-by: qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>

--- app/src/main/java/com/zaiming/android/lighthousegallery/extensions/ContextExtension.kt
+++ app/src/main/java/com/zaiming/android/lighthousegallery/extensions/ContextExtension.kt
@@ -2,8 +2,9 @@ package com.zaiming.android.lighthousegallery.extensions
 
 import android.content.Context
 import android.view.ContextThemeWrapper
+import androidx.annotation.StyleRes
 
 // function: add style to custom view constructor from drakeet
-fun Context.toTheme(themeResId: Int): Context {
+fun Context.toTheme(@StyleRes themeResId: Int): Context {
     return ContextThemeWrapper(this, themeResId)
 }
