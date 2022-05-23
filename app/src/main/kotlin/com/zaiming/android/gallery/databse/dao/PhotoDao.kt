cd038ba1f7c1f569c1622aecd9139d77bf06b25e
qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>
2022-05-23 10:52:27 +0800

    database extension
    Signed-off-by: qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>

--- app/src/main/kotlin/com/zaiming/android/gallery/databse/dao/PhotoDao.kt
+++ app/src/main/kotlin/com/zaiming/android/gallery/databse/dao/PhotoDao.kt
@@ -1,9 +1,6 @@
 package com.zaiming.android.gallery.databse.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.zaiming.android.gallery.databse.entity.MediaMetaData
 
 @Dao
@@ -13,6 +10,12 @@ interface PhotoDao {
     suspend fun getAllMediaMetaData(): List<MediaMetaData>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insetMediaMetaData(data: MediaMetaData)
+    suspend fun insetMediaMetaData(data: List<MediaMetaData>)
+
+    @Delete
+    suspend fun deleteMediaMetaData(data: List<MediaMetaData>)
+
+    @Query("SELECT * FROM media_metadata WHERE uri = (:uri)")
+    suspend fun queryMediaMetaDataFromUri(uri: String): MediaMetaData
 
 }
--- app/src/main/kotlin/com/zaiming/android/gallery/repository/PhotoDatabaseRepository.kt
+++ app/src/main/kotlin/com/zaiming/android/gallery/repository/PhotoDatabaseRepository.kt
@@ -1,7 +1,9 @@
 package com.zaiming.android.gallery.repository
 
+import android.net.Uri
 import com.zaiming.android.gallery.databse.dao.PhotoDao
 import com.zaiming.android.gallery.databse.entity.MediaMetaData
+import com.zaiming.android.gallery.extensions.divideBy
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,7 +15,19 @@ class PhotoDatabaseRepository @Inject constructor(private val photoDao: PhotoDao
         return photoDao.getAllMediaMetaData()
     }
 
-    suspend fun insertMediaMetaData(item: MediaMetaData) {
-        photoDao.insetMediaMetaData(item)
+    suspend fun insertMediaMetaData(vararg item: MediaMetaData) {
+        item.toList().divideBy().forEach {
+            photoDao.insetMediaMetaData(it)
+        }
+    }
+
+    suspend fun deleteMediaMetaData(vararg item: MediaMetaData) {
+        item.toList().divideBy().forEach {
+            photoDao.deleteMediaMetaData(it)
+        }
+    }
+
+    suspend fun queryMediaMetaDataFromUri(uri: Uri): MediaMetaData {
+        return photoDao.queryMediaMetaDataFromUri(uri.toString())
     }
 }
