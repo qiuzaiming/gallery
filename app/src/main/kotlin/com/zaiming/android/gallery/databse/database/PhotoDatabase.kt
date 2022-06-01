8721bd9deb062d8c3a8bac357a6dd8a7d5c01987
qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>
2022-06-01 09:32:20 +0800

    create single: double check
    Signed-off-by: qiuzaiming <51400371+qiuzaiming@users.noreply.github.com>

--- app/src/main/kotlin/com/zaiming/android/gallery/databse/database/PhotoDatabase.kt
+++ app/src/main/kotlin/com/zaiming/android/gallery/databse/database/PhotoDatabase.kt
@@ -26,17 +26,11 @@ abstract class PhotoDatabase : RoomDatabase() {
         private var INSTANCE: PhotoDatabase? = null
 
         fun getDatabase(context: Context): PhotoDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(context.applicationContext,
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(context.applicationContext,
                     PhotoDatabase::class.java,
                     "photo_database")
-                    .build()
-                INSTANCE = instance
-                return instance
+                    .build().also { INSTANCE = it }
             }
         }
     }
